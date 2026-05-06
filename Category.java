bb31cea24ceaebb30492c447b187a4e3ca8551ee
famefox2 <54617980+famefox2@users.noreply.github.com>
2026-05-06 16:33:58 -0700

    Add files via upload
    Starting enum for category, may update again

---
+++ Category.java
@@ -0,0 +1,14 @@
+package com.canigetafiver.lifemyway.web;
+
+import com.canigetafiver.lifemyway.service.UserService;
+
+public enum Category {
+    ENTERTAINMENT,
+    FOOD,
+    HEALTH,
+    RENT,
+    TRANSPORTATION,
+    UTILITIES,
+    OTHER
+
+}
